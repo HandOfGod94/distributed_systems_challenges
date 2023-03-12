e61b6d6297586de8d62e8abe7b5000a233459bd8
Gahan Rakholia <gahan94rakh@gmail.com>
2023-03-12 16:40:17 +0530

    use simple map instead of tset to form response

--- echoserver/fnl/echo.fnl
+++ echoserver/fnl/echo.fnl
@@ -4,16 +4,22 @@
 (local inspect (require :inspect))
 (local tablex (require :pl.tablex))
 
-(fn reply [node-id input resp]
+(fn handle-echo [node-id input]
   (let [{: src : body} input
-        {: msg_id} body
-        reply-body (tablex.merge body resp true)]
-    (tset reply-body :msg_id (+ msg_id 1))
-    (tset reply-body :in_reply_to msg_id)
-    (tset input :src node-id)
-    (tset input :dest src)
-    (tset input :body reply-body)
-    input))
+        {: msg_id} body]
+    {:src node-id
+     :dest src
+     :body (tablex.merge body {:msg_id (+ msg_id 1)
+                               :in_reply_to msg_id
+                               :type :echo_ok}
+                         true)}))
+
+(fn handle-init [node-id input]
+  (let [{: src : body} input
+        {: msg_id} body]
+    {:src node-id
+     :dest src
+     :body {:msg_id (+ msg_id 1) :in_reply_to msg_id :type :init_ok}}))
 
 (fn main []
   (var node-id nil)
@@ -27,13 +33,12 @@
       (match type
         :init (do
                 (io.stderr:write (.. "initialized node " node-id "\n"))
-                (-> (reply node-id input {:type :init_ok})
+                (-> (handle-init node-id input)
                     (cjson.encode)
                     (print)))
         :echo (do
                 (io.stderr:write (.. "Echoing body on node " node-id "\n"))
-                (-> node-id
-                    (reply input (tablex.merge body {:type :echo_ok} true))
+                (-> (handle-echo node-id input)
                     (cjson.encode)
                     (print)))))))
 
--- echoserver/lua/echo.lua
+++ echoserver/lua/echo.lua
@@ -1,40 +1,42 @@
 local cjson = require("cjson")
 local inspect = require("inspect")
 local tablex = require("pl.tablex")
-local function reply(node_id, input, resp)
+local function handle_echo(node_id, input)
   local _let_1_ = input
   local src = _let_1_["src"]
   local body = _let_1_["body"]
   local _let_2_ = body
   local msg_id = _let_2_["msg_id"]
-  local reply_body = tablex.merge(body, resp, true)
-  do end (reply_body)["msg_id"] = (msg_id + 1)
-  do end (reply_body)["in_reply_to"] = msg_id
-  input["src"] = node_id
-  input["dest"] = src
-  input["body"] = reply_body
-  return input
+  return {src = node_id, dest = src, body = tablex.merge(body, {msg_id = (msg_id + 1), in_reply_to = msg_id, type = "echo_ok"}, true)}
+end
+local function handle_init(node_id, input)
+  local _let_3_ = input
+  local src = _let_3_["src"]
+  local body = _let_3_["body"]
+  local _let_4_ = body
+  local msg_id = _let_4_["msg_id"]
+  return {src = node_id, dest = src, body = {msg_id = (msg_id + 1), in_reply_to = msg_id, type = "init_ok"}}
 end
 local function main()
   local node_id = nil
   while true do
     local input = cjson.decode(io.read("*l"))
-    local _let_3_ = input
-    local body = _let_3_["body"]
-    local _let_4_ = body
-    local node_id0 = _let_4_["node_id"]
-    local type = _let_4_["type"]
+    local _let_5_ = input
+    local body = _let_5_["body"]
+    local _let_6_ = body
+    local node_id0 = _let_6_["node_id"]
+    local type = _let_6_["type"]
     if (nil == node_id) then
       node_id = node_id0
     else
     end
-    local _6_ = type
-    if (_6_ == "init") then
+    local _8_ = type
+    if (_8_ == "init") then
       do end (io.stderr):write(("initialized node " .. node_id .. "\n"))
-      print(cjson.encode(reply(node_id, input, {type = "init_ok"})))
-    elseif (_6_ == "echo") then
+      print(cjson.encode(handle_init(node_id, input)))
+    elseif (_8_ == "echo") then
       do end (io.stderr):write(("Echoing body on node " .. node_id .. "\n"))
-      print(cjson.encode(reply(node_id, input, tablex.merge(body, {type = "echo_ok"}, true))))
+      print(cjson.encode(handle_echo(node_id, input)))
     else
     end
   end
