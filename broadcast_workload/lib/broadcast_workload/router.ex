cfb32ac41482e4f3d421e74469fd1ecfcbc86910
Gahan Rakholia <gahan94rakh@gmail.com>
2023-03-26 12:54:56 +0530

    add non-performant elixir genserver broadcast server

--- broadcast_workload/lib/broadcast_workload/router.ex
+++ broadcast_workload/lib/broadcast_workload/router.ex
@@ -7,10 +7,11 @@ defmodule BroadcastWorkload.Router do
   end
 
   def dispatch(command) do
-    with {:ok, result} <- GenServer.call(__MODULE__, command),
+    with {:ok, %{} = result} <- GenServer.call(__MODULE__, command),
          {:ok, result} <- Jason.encode(result) do
       IO.puts(result)
     else
+      {:ok, :noop} -> IO.puts(:stderr, "idempotent message")
       {:error, error} -> IO.puts(:stderr, error)
     end
   end
@@ -18,7 +19,7 @@ defmodule BroadcastWorkload.Router do
   # server callbacks
   @impl GenServer
   def init(_) do
-    {:ok, %{node_id: nil, topology: [], messages: MapSet.new()}}
+    {:ok, %{node_id: nil, neighbours: [], messages: MapSet.new()}}
   end
 
   @impl GenServer
@@ -38,6 +39,7 @@ defmodule BroadcastWorkload.Router do
   @impl GenServer
   def handle_call(%{body: %{type: "topology"}} = input, _from, state) do
     %{src: dest, body: %{topology: topology, msg_id: msg_id}} = input
+    IO.puts(:stderr, "initializing topology #{inspect(topology)}")
 
     {:reply,
      {:ok,
@@ -45,20 +47,34 @@ defmodule BroadcastWorkload.Router do
         src: state.node_id,
         dest: dest,
         body: %{type: "topology_ok", in_reply_to: msg_id, msg_id: msg_id + 1}
-      }}, %{state | topology: topology}}
+      }}, %{state | neighbours: topology[String.to_atom(state.node_id)]}}
   end
 
   @impl GenServer
   def handle_call(%{body: %{type: "broadcast"}} = input, _from, state) do
-    %{src: dest, body: %{message: message, msg_id: msg_id}} = input
+    %{src: dest, body: %{message: message, msg_id: msg_id} = body} = input
 
-    {:reply,
-     {:ok,
-      %{
-        src: state.node_id,
-        dest: dest,
-        body: %{type: "broadcast_ok", in_reply_to: msg_id, msg_id: msg_id + 1}
-      }}, %{state | messages: MapSet.put(state.messages, message)}}
+    if MapSet.member?(state.messages, message) || dest == state.node_id do
+      {:reply, {:ok, :noop}, state}
+    else
+      IO.puts(
+        :stderr,
+        "broadcasting to neighbours #{inspect(state.neighbours)} from #{state.node_id}"
+      )
+
+      state
+      |> Map.get(:neighbours, [])
+      |> Enum.filter(&(&1 != state.node_id))
+      |> Enum.each(&request(state.node_id, &1, body))
+
+      {:reply,
+       {:ok,
+        %{
+          src: state.node_id,
+          dest: dest,
+          body: %{type: "broadcast_ok", in_reply_to: msg_id, msg_id: msg_id + 1}
+        }}, %{state | messages: MapSet.put(state.messages, message)}}
+    end
   end
 
   @impl GenServer
@@ -84,5 +100,11 @@ defmodule BroadcastWorkload.Router do
      {:error,
       "unknown command recieved #{inspect(unknown_input)}. Current state: #{inspect(state)}"},
      state}
+  end
+
+  defp request(src, dest, body) do
+    %{src: src, dest: dest, body: body}
+    |> Jason.encode!()
+    |> IO.puts()
   end
 end
